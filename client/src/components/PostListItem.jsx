83541bef248be9d74c2910ab019d768b79256a4b
airnesygroup <airnesy.info@gmail.com>
2024-12-07 16:31:41 +0300

    second  commit

--- client/src/components/PostListItem.jsx
+++ client/src/components/PostListItem.jsx
@@ -3,9 +3,8 @@ import Image from "./Image";
 import { format } from "timeago.js";
 
 const PostListItem = ({ post }) => {
-
   return (
-    <div className="flex flex-col xl:flex-row gap-8 mb-12">
+    <div className="flex flex-col xl:flex-row gap-6 sm:gap-8 mb-8 sm:mb-12">
       {/* image */}
       {post.img && (
         <div className="md:hidden xl:block xl:w-1/3">
@@ -13,19 +12,32 @@ const PostListItem = ({ post }) => {
         </div>
       )}
       {/* details */}
-      <div className="flex flex-col gap-4 xl:w-2/3">
-        <Link to={`/${post.slug}`} className="text-4xl font-semibold">
+      <div className="flex flex-col gap-3 sm:gap-4 xl:w-2/3">
+        <Link
+          to={`/${post.slug}`}
+          className="text-lg sm:text-xl lg:text-2xl font-semibold"
+        >
           {post.title}
         </Link>
-        <div className="flex items-center gap-2 text-gray-400 text-sm">
+        <div className="flex flex-wrap items-center gap-1 sm:gap-2 text-gray-400 text-xs sm:text-sm">
           <span>Written by</span>
-          <Link className="text-blue-800" to={`/posts?author=${post.user.username}`}>{post.user.username}</Link>
+          <Link
+            className="text-blue-800 text-xs sm:text-sm"
+            to={`/posts?author=${post.user.username}`}
+          >
+            {post.user.username}
+          </Link>
           <span>on</span>
-          <Link className="text-blue-800">{post.category}</Link>
+          <Link className="text-blue-800 text-xs sm:text-sm">
+            {post.category}
+          </Link>
           <span>{format(post.createdAt)}</span>
         </div>
-        <p>{post.desc}</p>
-        <Link to={`/${post.slug}`} className="underline text-blue-800 text-sm">
+        <p className="text-sm sm:text-base text-gray-700">{post.desc}</p>
+        <Link
+          to={`/${post.slug}`}
+          className="underline text-blue-800 text-xs sm:text-sm"
+        >
           Read More
         </Link>
       </div>
