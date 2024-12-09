26434ead8cc07fd0e3631e346cd3d967643b95fd
airnesygroup <airnesy.info@gmail.com>
2024-12-09 14:15:03 +0300

    second  commit

--- client/src/routes/Homepage.jsx
+++ client/src/routes/Homepage.jsx
@@ -9,7 +9,7 @@ const Homepage = () => {
   
   <div className="flex items-center justify-center">
   {/* Titles */}
-  <h1 className="text-gray-800 mb-9 mt-9 text-2xl md:text-3xl lg:text-8xl font-bold text-center">
+  <h1 className="text-gray-800 mb-9 mt-9 text-4xl md:text-5xl lg:text-8xl font-bold text-center">
      <span className="text-transparent bg-clip-text bg-gradient-to-r from-black to-[#1DA1F2]">THE VERGE</span>
   </h1>
 </div>
