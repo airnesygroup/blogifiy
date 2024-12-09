ddaaad8a3bffee24ce40d274027f329a9960fe65
airnesygroup <airnesy.info@gmail.com>
2024-12-09 18:07:11 +0300

    second  commit

--- client/src/routes/Homepage.jsx
+++ client/src/routes/Homepage.jsx
@@ -10,7 +10,7 @@ const Homepage = () => {
   <div className="flex items-center justify-center">
   {/* Titles */}
   <h1 className="text-gray-800 mb-9 mt-9 text-4xl md:text-5xl lg:text-8xl font-bold text-center">
-     <span className="text-transparent bg-clip-text bg-gradient-to-r from-black to-[#1DA1F2]">HOOLICON</span>
+     <span className="text-transparent bg-clip-text bg-gradient-to-r from-black to-[#1DA1F2]">INVERSE</span>
   </h1>
 </div>
 
