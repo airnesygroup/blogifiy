db78d8264b3d99ea37e55eccdca14d2c16f1277a
airnesygroup <airnesy.info@gmail.com>
2024-12-09 22:11:11 +0300

    second  commit

--- client/src/routes/Homepage.jsx
+++ client/src/routes/Homepage.jsx
@@ -5,14 +5,20 @@ import PostList from "../components/PostList";
 
 const Homepage = () => {
   return (
+
+    
     <div className=" mb-8 flex flex-col gap-0">
   
   <div className="flex items-center justify-center">
   {/* Titles */}
-
+  <h1 className="text-gray-800 mb-9 mt-9 text-4xl md:text-5xl lg:text-8xl font-bold text-center">
+     <span className="text-transparent bg-clip-text bg-gradient-to-r from-white to-[#1DA1F2]">The Hooli</span>
+  </h1>
 </div>
 
+
       {/* CATEGORIES */}
+      <MainCategories />
       {/* FEATURED POSTS */}
       <FeaturedPosts />
       {/* POST LIST */}
