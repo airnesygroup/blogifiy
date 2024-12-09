2074928053628a48e640cb213b7088f66f4a313f
airnesygroup <airnesy.info@gmail.com>
2024-12-09 16:13:32 +0300

    second  commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,27 @@
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 
 const App = () => {
+  useEffect(() => {
+    // Function to handle scroll and update the gradient position
+    const handleScroll = () => {
+      const gradient = document.body;
+      if (window.scrollY > 1000) {
+        gradient.style.setProperty("--gradient-position", "fixed");
+      } else {
+        gradient.style.setProperty("--gradient-position", "absolute");
+      }
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
       {/* NAVBAR */}
