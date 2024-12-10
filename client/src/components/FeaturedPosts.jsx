b4524b41afacb0f73586bcbf415114e12c8cd138
airnesygroup <airnesy.info@gmail.com>
2024-12-10 19:29:33 +0300

    second  commit

--- client/src/components/FeaturedPosts.jsx
+++ client/src/components/FeaturedPosts.jsx
@@ -130,7 +130,7 @@ ext-xl font-semibold bg-gradient-to-r bg-gradient-to-r bg-gradient-to-r bg-gradi
       BY THE <br />
       <span className="underline">NUMBERS</span>
     </p>
-    <p className="text-5xl font-extrabold bg-clip-text text-transparent  bg-gradient-to-r from-[#1DA1F2]  to-white">
+    <p className="text-5xl font-extrabold bg-clip-text text-transparent  bg-gradient-to-r from-white  to-[#1DA1F2] ">
       <span style={{ fontSize: '1.2rem' }}>OVER</span> 100K+
     </p>
     <p className="text-white" style={{ fontSize: '1rem' }}>
