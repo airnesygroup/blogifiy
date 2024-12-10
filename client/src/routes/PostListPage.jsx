0352a2b9081bb578f29ca42262b432d8dea6924f
airnesygroup <airnesy.info@gmail.com>
2024-12-10 17:33:44 +0300

    second  commit

--- client/src/routes/PostListPage.jsx
+++ client/src/routes/PostListPage.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
+import { useRouter } from "next/router"; // Import useRouter
 import PostList from "../components/PostList";
 import SideMenu from "../components/SideMenu";
 
 const PostListPage = () => {
   const [open, setOpen] = useState(false);
+  const router = useRouter(); // Initialize router
+
+  // Get the current search query parameters
+  const { query } = router;
+  
+  // You can format the query object to display a more readable value
+  const searchParam = query.search || "All Posts"; // Default to "All Posts" if no search parameter exists
 
   return (
     <div className="">
-      <h1  style={{ paddingTop: "100px",  }}
-       className="mb-8 font-bold text-2xl">Development Blog</h1>
+      <h1 style={{ paddingTop: "100px" }} className="mb-8 font-bold text-2xl">
+        {`Development Blog - Search Results for: "${searchParam}"`}
+      </h1>
       <button
         onClick={() => setOpen((prev) => !prev)}
         className="bg-blue-800 text-sm text-white px-4 py-2 rounded-2xl mb-4 md:hidden"
