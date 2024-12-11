da0a8bd3ba18a1b39d806a684578083ad6af99a8
airnesygroup <airnesy.info@gmail.com>
2024-12-11 13:48:30 +0300

    second  commit

--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -23,7 +23,7 @@ const SideMenu = () => {
 
 
   return (
-    <div className="px-4 h-max sticky top-8">
+    <div className="px-4 h-max top-8">
       <h1 className="mb-4 text-sm font-medium">Search</h1>
       <Search />
       <h1 className="mt-8 mb-4 text-sm font-medium">Filter</h1>
