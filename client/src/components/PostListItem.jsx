cc2f089e009112256ee9173805ccabf2f3779a5e
airnesygroup <airnesy.info@gmail.com>
2024-12-13 16:29:04 +0300

    second  commit

--- client/src/components/PostListItem.jsx
+++ client/src/components/PostListItem.jsx
@@ -4,8 +4,8 @@ import { format } from "timeago.js";
 
 const PostListItem = ({ post }) => {
   // Truncate title based on screen size
-  const truncatedTitle = post.title.length > 100 
-    ? `${post.title.substring(0, 100)}...` 
+  const truncatedTitle = post.title.length > 90
+    ? `${post.title.substring(0, 90)}...` 
     : post.title;
 
   const truncatedTitleLarge = post.title.length > 150 
@@ -38,8 +38,8 @@ const PostListItem = ({ post }) => {
   <span className="hidden md:inline">{post.title}</span>
 </h3>
 
-<div className="flex flex-wrap items-center gap-1 sm:gap-2     text-[var(--softTextColor)]
- md:text-[12px] lg:text-[14px] md:top-[-10px] lg:relative lg:top-[-25px]">
+<div className="flex flex-wrap items-center gap-1 sm:gap-2 text-[var(--softTextColor)]  text-[11px] sm:text-[11px]
+ md:text-[12px] lg:text-[14px] md:top-[-10px] lg:relative lg:top-[-35px]">
   <span> By</span>
   <Link
     className="text-[#1DA1F2] text-[11px] sm:text-[11px] md:[12px] lg:text-[13px]"
