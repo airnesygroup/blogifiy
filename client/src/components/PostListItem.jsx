51aa9062c22e8b1781bcac10a518531fea1a7fe1
airnesygroup <airnesy.info@gmail.com>
2024-12-16 13:51:45 +0300

    second  commit

--- client/src/components/PostListItem.jsx
+++ client/src/components/PostListItem.jsx
@@ -34,7 +34,7 @@ const PostListItem = ({ post }) => {
       <div className="flex flex-col justify-between">
       <h3 className="text-[14px] sm:text-[14px] md:[27px] lg:[27px] text-[var(--textColor)]  ">
   <span className="sm:hidden">{truncatedTitle}</span>
-  <span className="hidden sm:inline md:hidden">{truncatedTitleLarge}</span>
+  <span className="text-[17px] hidden sm:inline md:hidden">{truncatedTitleLarge}</span>
   <span className="hidden md:inline">{post.title}</span>
 </h3>
 
