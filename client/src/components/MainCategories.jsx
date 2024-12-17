ad2bc3ab3b16d5e2b3a9272d755635dd6880b09f
airnesygroup <airnesy.info@gmail.com>
2024-12-17 03:35:18 +0300

    second  commit

--- client/src/components/MainCategories.jsx
+++ client/src/components/MainCategories.jsx
@@ -11,46 +11,46 @@ const MainCategories = () => {
       {/* Navigation container */}
       <div className="flex items-center text-[#e6e6ff] justify-between px-4 py-2">
         {/* Categories or Search Bar */}
-        <div className="flex flex-1 items-center">
+        <div className="flex flex-1  items-center">
           {!isSearchOpen ? (
             <div
               className={`${
                 isMenuOpen ? "hidden" : "hidden"
-              } lg:flex items-center flex-wrap overflow-x-auto whitespace-nowrap`}
+              } lg:flex items-center flex-wrap overflow-x-auto hidden sm:hidden md:block whitespace-nowrap`}
             >
               <Link
-                to="/posts"
+                to="/posts?sort=newest"
                 className="bg-black  text-white rounded-full px-4 py-3"
               >
                 Latest
               </Link>
               <Link
-                to="/posts?cat=web-design"
-                className="hover:bg-blue-50 rounded-full px-4 py-1"
+                to="/posts?sort=popular"
+                className="hover:bg-black rounded-full px-4 py-1"
               >
                 Most Popular
               </Link>
               <Link
-                to="/posts?cat=development"
-                className="hover:bg-blue-50 rounded-full px-4 py-1"
+                to="/posts?sort=trending"
+                className="hover:bg-black rounded-full px-4 py-1"
               >
                 Top Trending
               </Link>
               <Link
-                to="/posts?cat=databases"
-                className="hover:bg-blue-50 rounded-full px-4 py-1"
+                to="/posts?sort=oldest"
+                className="hover:bg-black rounded-full px-4 py-1"
               >
                 Oldest
               </Link>
               <Link
-                to="/posts?cat=seo"
-                className="hover:bg-blue-50 rounded-full px-4 py-1"
+                to="/newsletter"
+                className="hover:bg-black rounded-full px-4 py-1"
               >
               Newsletter            
               </Link>
               <Link
-                to="/posts?cat=marketing"
-                className="hover:bg-blue-50 rounded-full px-4 py-1"
+                to="/about"
+                className="hover:bg-black  rounded-full px-4 py-1"
               >
                 About Us
               </Link>
@@ -66,8 +66,24 @@ const MainCategories = () => {
 
         {/* Right-side icons and controls */}
         <div className="flex items-center py-[0.8px] space-x-4">
+
+      
+
           {/* User and Sign In */}
           <div className="flex items-center space-x-2">
+
+        
+
+
+              <Link
+              to="/newsletter"
+              className={`bg-black  text-white  sm:block md:hidden  rounded-full px-4 py-3 ${
+                isSearchOpen ? "hidden" : "block"
+              }`}
+            >  
+                Newsletter
+              </Link> 
+
             <Link
               to="/signin"
               className={`px-4 py-3  md:py-2 flex flex-row border-white text-xs  md:text-sm text-white ${
@@ -99,7 +115,7 @@ const MainCategories = () => {
 
       {/* Categories dropdown for menu */}
       {isMenuOpen && !isSearchOpen && (
-        <div className="absolute top-full left-0 w-full bg-gray-800 p-4 z-10">
+        <div className="absolute top-full left-0 w-full bg-[#1da1f2] p-4 z-10">
           <Link
             to="/posts"
             className="block py-2 text-white hover:bg-gray-700 rounded"
