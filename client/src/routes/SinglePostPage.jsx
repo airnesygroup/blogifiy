635eabed9097057f25d9312e2dc006f63275d1f8
airnesygroup <airnesy.info@gmail.com>
2024-12-19 22:46:41 +0300

    second  commit

--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -25,7 +25,7 @@ const SinglePostPage = () => {
   if (!data) return "Post not found!";
 
   return (
-    <div className="mx-auto p-0 md:p-4 flex flex-col items-center gap-2 md:gap-4">
+    <div className="mx-auto p-0 md:p-4 flex flex-col items-center gap-4 md:gap-8">
 
 
 <div  style={{ zIndex: 100004 }} className=" mt-[15px] sticky top-0.5 md:top-2 ">
