2b2bd7b3863358bf80fe97480d5e52aa4b4b2dc1
airnesygroup <airnesy.info@gmail.com>
2024-12-19 22:17:34 +0300

    second  commit

--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -25,6 +25,11 @@ const SinglePostPage = () => {
 
   return (
     <div className="mx-auto p-4 flex flex-col items-center gap-8">
+
+
+<div  style={{ zIndex: 100004 }} className="mb-[45px] md:mb-[50px] mt-[15px] md:mt-[20px] sticky top-0.5 md:top-2 ">
+ <MainCategories/>
+</div>
       {/* Content container */}
       <div className="w-full max-w-[700px]">
         {/* Title */}
