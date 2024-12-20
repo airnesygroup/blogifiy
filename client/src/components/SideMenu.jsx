e86560dcf278c9c30d1751ab5cc1ee0249a2975f
airnesygroup <airnesy.info@gmail.com>
2024-12-20 11:55:21 +0300

    second  commit

--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -30,7 +30,7 @@ const SideMenu = () => {
         maxHeight: "100vh",
         overflowY: "auto", // Make it scrollable if content overflows
       }}
-      className="px-6  py-4 bg-[var(--bg)]  border-2 border-[var(--softBgMenu2)]  rounded-lg  sticky top-[120px] flex flex-col gap-4 text-[var(--textColor)] shadow-md"
+      className="px-6  py-4 bg-[var(--bg)]  border-2 border-[var(--softTextColor7)]  rounded-lg  sticky top-[120px] flex flex-col gap-4 text-[var(--textColor)] shadow-md"
     >
       <div>
         <h1 className="mt-2 mb-4 text-md font-semibold text-[var(--textColor)]">Filter</h1>
